b3726286a6b7b378296e16eb18af01a71457158e
PensActius <37923848+pensactius@users.noreply.github.com>
2020-07-20 19:14:13 +0200

    Update u8g2_hello_world.ino

--- arduino/u8g2_hello_world/u8g2_hello_world.ino
+++ arduino/u8g2_hello_world/u8g2_hello_world.ino
@@ -2,7 +2,9 @@
 #include <U8g2lib.h>
 #include <SPI.h>
 
-
+// Descomentar la línea correspondiente al modelo de pantalla a usar
+// En PensActius, el modelo de 1.3 pulgadas es el modelo SH1106, 
+// y la de 0.96 pulgadas es el modelo SD1306.
 U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=ss*/ 10, /* dc=*/ 9, /* reset=*/ 8);
 //U8G2_SH1106_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
 
